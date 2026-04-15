64474bd037fe6cda434c85a5236ddcfa5426e482
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-04-15 14:36:10 +0000

    feat: add all correspondence models - Department - CustomUser -Incoming -Dispatch - Letters

--- correspondence/admin.py
+++ correspondence/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Department)
+admin.site.register(CustomUser)
+admin.site.register(IncomingCorrespondence)
+admin.site.register(Dispatch)
+admin.site.register(Letters)
+
 
 # Register your models here.
