73d4bd56272d2f31856623818bd2112990d73297
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-04-29 13:40:24 +0000

    update settings.py

--- CMS/settings.py
+++ CMS/settings.py
@@ -98,6 +98,7 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),  
         'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {'sslmode': os.getenv('sslmode')},
     }
 }
 
