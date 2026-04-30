f300b9d95af872f16d4fa9d506c7f1c145e0a933
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-04-30 07:45:17 +0000

    fix: fix admin so passwords can be hashed

--- correspondence/admin.py
+++ correspondence/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-from .models import *
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Department, IncomingCorrespondence, Dispatch, Letters
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'first_name', 'last_name', 'department', 'is_active', 'is_staff']
+    list_filter = ['is_active', 'is_staff', 'department']
+    search_fields = ['email', 'username', 'first_name', 'last_name']
+    ordering = ['email']
+
+    # Fields shown when EDITING an existing user
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'department')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    # Fields shown when CREATING a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'department', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Department)
-admin.site.register(CustomUser)
 admin.site.register(IncomingCorrespondence)
 admin.site.register(Dispatch)
 admin.site.register(Letters)
-
-
-# Register your models here.
